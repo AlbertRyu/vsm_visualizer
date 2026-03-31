11cd205b4612c6431b1956fbeb93fbf0e47005ae
yunxiao_liu <yunxiao.liu@tum.de>
2026-03-31 15:44:04 +0200

    add support for custom folder when start in command line

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 from pathlib import Path
 import sys
 
+from vsm_visualizer.ui import run_app
 
-print('something')
+
 def get_exe_dir():
     if "__compiled__" in globals():
         return Path(sys.argv[0]).resolve().parent
     return Path(__file__).resolve().parent
 
-ROOT = get_exe_dir()
-
-
-from vsm_visualizer.ui import run_app
-
 if __name__ == "__main__":
-
-    run_app(start_dir=ROOT)
-    
+    if len(sys.argv) > 1:
+        start_dir = Path(sys.argv[1]).resolve()
+    else:
+        start_dir = get_exe_dir()
+    run_app(start_dir=start_dir)
